3a4c8e383663ebb1ba63da4678ca722290d51949
harupy <hkawamura0130@gmail.com>
2019-10-29 11:18:09 +0900

    update

---
+++ hello_world_nbconvert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def say_hello_world():
+    print('hello world')
+
+
+# In[2]:
+
+
+say_hello_world()
+
+
+# ### Hello World
+
+# In[ ]:
+
+
+
+
